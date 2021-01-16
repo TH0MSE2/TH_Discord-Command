a2e33b32304243fd7ad0b9f11bb9ff6c6393f5f1
TH0MSE2 <67968802+TH0MSE2@users.noreply.github.com>
2021-01-16 10:50:33 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,7 +1,4 @@
-
-
 Citizen.CreateThread(function() TriggerEvent('chat:addSuggestion', '/discord', 'Viser discord link.') end)
 RegisterCommand("discord", function(source, args, rawCommandString)
     TriggerEvent('chatMessage', "^1[Server Name]", { 255, 0, 62 }, "Discord link")
 end, false)
-
